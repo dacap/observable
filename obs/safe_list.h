dacap/observable

commit f9288e0ae79ad4445cfb0c1308a4c46e273a99a8

Simplify safe_list::iterator::operator!=() for its unique use case
We have a data race condition between safe_list::iterator::operator!=()
and safe_list::push_back() with m_last->next member. Instead of locking
m_list.m_mutex_nodes for this simple case (which reduce the performance
significantly), we can just compare the node with nullptr (we know that
m_last->next is nullptr and iterator::operator!= is used only with the
end() iterator).

obs/safe_list.h
@@ -234,9 +234,9 @@ public:
 
     // This can be used only to compare an iterator created from
     // begin() (in "this" pointer) with end() ("other" argument).
-    bool operator!=(const iterator& other) const {
-      if (m_node && other.m_node)
-        return (m_node != other.m_node->next);
+    bool operator!=(const iterator&) const {
+      if (m_node)
+        return (m_node != nullptr);
       else
         return false;
     }

commit 8ff3884bd4ed792cbd2d73fe78e993f893687ef4

Fix reconnect_on_notification test
Regression introduced in f9288e0ae79ad4445cfb0c1308a4c46e273a99a8

We need to compare the end()'s node->next value to avoid iterating over
and over again when we reconnect the signal from the signal itself.

obs/safe_list.h
@@ -235,9 +235,10 @@ public:
 
     // This can be used only to compare an iterator created from
     // begin() (in "this" pointer) with end() ("other" argument).
-    bool operator!=(const iterator&) const {
-      if (m_node)
-        return (m_node != nullptr);
+    bool operator!=(const iterator& other) const {
+      std::lock_guard<std::mutex> lock(m_list.m_mutex_nodes);
+      if (m_node && other.m_node)
+        return (m_node != other.m_node->next);
       else
         return false;
     }